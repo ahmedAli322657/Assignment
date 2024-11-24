681e2410589207bc7f4b94a5353a8656b6f842bc
ahmedAli322657 <aa0017977@gmail.com>
2024-11-24 19:04:42 +0200

    Last commit

--- js/js.js
+++ js/js.js
@@ -19,12 +19,17 @@ function createUrl() {
     localStorage.setItem('sitelist', JSON.stringify(siteList));
     displaySite();
     clear();
+    siteName.classList.remove("is-valid");
+    siteName.classList.remove("is-invalid");
+
+    siteUrl.classList.remove("is-valid");
+    siteUrl.classList.remove("is-invalid");
 }
 
 function displaySite() {
     var cartona = '';
 
-    for (var i = 0; i < siteList.length; i++) {
+    for (var i = 1; i < siteList.length; i++) {
         cartona += `<tr class="fw-medium border-bottom">
                         <td>${[i]}</td>
                         <td>${siteList[i].name}</td>
@@ -46,4 +51,34 @@ function deleteSite(index) {
     siteList.splice(index, 1);
     localStorage.setItem('sitelist', JSON.stringify(siteList));
     displaySite();
+}
+
+function validateSiteName() {
+    var regex = /^[a-z]{3,}$/;
+    if (regex.test(siteName.value)) {
+        siteName.classList.add("is-valid");
+        siteName.classList.remove("is-invalid");
+
+    } else {
+        siteName.classList.add("is-invalid");
+        siteName.classList.remove("is-valid");
+
+    }
+}
+
+function validateUrl() {
+    // var urlRegex = /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z0-9]-*)*[a-zA-Z0-9]+)(?:\.(?:[a-zA-Z0-9]-*)*[a-zA-Z0-9]+)*(?:\.(?:[a-zA-Z]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    var urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/
+
+    if (urlRegex.test(siteUrl.value)) {
+        console.log("match");
+        siteUrl.classList.add("is-valid");
+        siteUrl.classList.remove("is-invalid");
+
+    } else {
+        console.log("not match");
+        siteUrl.classList.add("is-invalid");
+        siteUrl.classList.remove("is-valid");
+
+    }
 }
